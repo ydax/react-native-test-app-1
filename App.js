6161a8f70988831ccad02ed484ca0fcd12466d2a
pdavisjones <pdavisjones@gmail.com>
2020-02-08 16:50:12 -0600

    basics of styles and buttons

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  // Creates State for the Component.
+  const [name, setName] = useState('Davis');
+
+  // Creates State for person.
+  const [person, setPerson] = useState({
+    name: 'John',
+    age: 33
+  })
+
+  // Handles button clicks.
+  const clickHandler = () => {
+    setName('Ludell');
+    setPerson({
+      name: 'Luigi',
+      age: 45
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.body}>
+        <Text>My name is {name}.</Text>
+        <Text>Her name is {person.name} and his age is {person.age}.</Text>
+        <View style={styles.buttonContainer}></View>
+        {// In React Native, a button is a self-closing tag with props to decorate it.
+        }
+        <Button title="update state" onPress={clickHandler} />
+        <Text></Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +43,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 20
+  },
+  body: {
+    padding: 20
+  }
 });
